12bf639e05c5f95f6638efe2c8ad4e4afbc62638
Runzhen Wang <wangrunzhen@gmail.com>
2026-04-29 21:11:50 -0700

    add icon

--- scripts/build-app.sh
+++ scripts/build-app.sh
@@ -9,9 +9,26 @@ swift build -c release
 APP="build/QuickOpen.app"
 rm -rf "$APP"
 mkdir -p "$APP/Contents/MacOS"
+mkdir -p "$APP/Contents/Resources"
 
 BIN="$(swift build -c release --show-bin-path)/quickopen"
 cp "$BIN" "$APP/Contents/MacOS/QuickOpen"
+
+# Build app icon from Image.png
+ICONSET="build/AppIcon.iconset"
+rm -rf "$ICONSET"
+mkdir -p "$ICONSET"
+sips -z 16 16     resources/icon.png --out "$ICONSET/icon_16x16.png"
+sips -z 32 32     resources/icon.png --out "$ICONSET/icon_16x16@2x.png"
+sips -z 32 32     resources/icon.png --out "$ICONSET/icon_32x32.png"
+sips -z 64 64     resources/icon.png --out "$ICONSET/icon_32x32@2x.png"
+sips -z 128 128   resources/icon.png --out "$ICONSET/icon_128x128.png"
+sips -z 256 256   resources/icon.png --out "$ICONSET/icon_128x128@2x.png"
+sips -z 256 256   resources/icon.png --out "$ICONSET/icon_256x256.png"
+sips -z 512 512   resources/icon.png --out "$ICONSET/icon_256x256@2x.png"
+sips -z 512 512   resources/icon.png --out "$ICONSET/icon_512x512.png"
+sips -z 1024 1024 resources/icon.png --out "$ICONSET/icon_512x512@2x.png"
+iconutil -c icns "$ICONSET" -o "$APP/Contents/Resources/AppIcon.icns"
 
 cat > "$APP/Contents/Info.plist" << 'PLIST'
 <?xml version="1.0" encoding="UTF-8"?>
@@ -30,6 +47,8 @@ cat > "$APP/Contents/Info.plist" << 'PLIST'
     <string>QuickOpen</string>
     <key>CFBundlePackageType</key>
     <string>APPL</string>
+    <key>CFBundleIconFile</key>
+    <string>AppIcon</string>
     <key>LSUIElement</key>
     <true/>
     <key>LSMinimumSystemVersion</key>
